10316d7ba98fda0fe8ac931e578e08f451ea5cf6
Stanislas Bertrand <sbertrand@witekio.com>
2019-06-23 10:30:38 -0700

    Make function definition consistent, debug if verbose is on

--- esp-sbfe-common/esp-sbfe-function.mk
+++ esp-sbfe-common/esp-sbfe-function.mk
@@ -2,8 +2,7 @@
 # ESP Secure Boot & Flash Encryption functions
 #
 
-$(info Definition of SBFE Function : $(MAKELEVEL))
-
+SBFE_VAR_DRY_RUN?=
 SBFE_FLH_DRYRUN=
 ifeq ($(SBFE_VAR_DRY_RUN),1)
 SBFE_FLH_DRYRUN=echo "\t\t"
@@ -28,6 +27,8 @@ SBFE_VAR_FLSH_IDX?=
 ifeq ($(MAKELEVEL),0)
 
 #
+# SBFE binary declaration is for variable related to the binary file
+#
 # Help to define SBFE Var to be exported
 # $1 index
 # $2 binary
@@ -35,34 +36,42 @@ ifeq ($(MAKELEVEL),0)
 # $4 encryption (1-enable)
 #
 define sbfe-declare-binary
-$(info SBFE declaration of binary $1)
+$(if $(V),$(info SBFE - declaration of binary variables for $1))
 $(eval SBFE_VAR_BIN_IDX+=$1)
 $(eval SBFE_VAR_BIN_$(1)_BIN=$2)
 $(eval SBFE_VAR_BIN_$(1)_OFFSET=$3)
 $(eval SBFE_VAR_BIN_$(1)_ENC=$4)
 $(eval SBFE_VAR_BIN_$(1)_SECBT=$5)
-$(call sbfe-declare-target,$1)
+$(call sbfe-declare-binary-target,$1)
 endef
 
-define sbfe-declare-target
-$(info SBFE declaration of binary target $1)
+#
+# SBFE binary target declaration is for calling flashing the binary
+#
+define sbfe-declare-binary-target
+$(if $(V),$(info SBFE - declaration of binary target for $1))
 sbfe-flash-$1: sbfe-flash-check sbfe-export $(SBFE_VAR_BIN_$(1)_BIN)
 	$$(SBFE_CMD_MAKE) $$@
 endef
 
 #
-# Help to define SBFE combined binary to flash
+# SBFE flash declaration is for variable related to multiple binary files
 #
-define sbfe-declare-flash-target
-$(info SBFE declaration of flash target $1)
+# Will reference different binary file that should be declared before
+#
+define sbfe-declare-flash
+$(if $(V),$(info SBFE - declaration of flash variable for $1))
 $(eval SBFE_VAR_FLSH_IDX+=$1)
 $(eval SBFE_VAR_FLSH_$(1)_TARGET=$1)
 $(eval SBFE_VAR_FLSH_$(1)_BIN_IDX=$2)
-$(call sbfe-declare-target-flash-target,$1)
+$(call sbfe-declare-flash-target,$1)
 endef
 
-define sbfe-declare-target-flash-target
-$(info SBFE declaration of binary target $1)
+#
+# SBFE flash target declaration is for calling flashing multiple binaries
+#
+define sbfe-declare-flash-target
+$(if $(V),$(info SBFE - declaration of flash target for $1))
 
 sbfe-flash-$1-dep: $(foreach bin,$(SBFE_VAR_FLSH_$(1)_BIN_IDX), $(SBFE_VAR_BIN_$(bin)_BIN))
 	echo Dependency :
@@ -74,11 +83,11 @@ endef
 
 else # Make level is 0
 
-define sbfe-declare-target
+define sbfe-declare-binary-target
 ifneq ($(SBFE_VAR_BIN_$(1)_BIN),)
 ifneq ($(SBFE_VAR_BIN_$(1)_OFFSET),)
 
-$(info SBFE declaration of binary target $1)
+$(if $(V),$(info SBFE - declaration of binary target $1))
 
 ifeq ($(SBFE_VAR_BIN_$(1)_SECBT).$(SBFE_VAR_SECURE_BOOT),1.1)
 # Save original filename
@@ -104,7 +113,7 @@ $(if $(filter $(SBFE_VAR_BIN_$(1)_SECBT).$(SBFE_VAR_SECURE_BOOT),1.1), \
 $(if $(filter $(SBFE_VAR_BIN_$(1)_SECBT).$(SBFE_VAR_SECURE_BOOT),1.1), \
 	$(eval SBFE_VAR_BIN_$(1)_OFFSET:=0))
 
-endif
+endif # Secure Boot check
 
 ifeq ($(SBFE_VAR_BIN_$(1)_ENC).$(SBFE_VAR_FLASH_ENCRYPTION),1.1)
 # Flashing with encrypted binary
@@ -122,11 +131,11 @@ $(SBFE_FLSH_BIN_$(1)_BIN_ENC): $(SBFE_VAR_BIN_$(1)_BIN) $(SBFE_VAR_KEY_DIR)/$(SB
 		
 SBFE_FLSH_BIN_$(1)_DEP=$(SBFE_FLSH_BIN_$(1)_BIN_ENC)
 SBFE_FLSH_BIN_$(1)_ARG=$(SBFE_VAR_BIN_$(1)_OFFSET) $(SBFE_FLSH_BIN_$(1)_BIN_ENC)
-else
+else # Flash Encryption enabled
 # Flashing directly binary
 SBFE_FLSH_BIN_$(1)_DEP=$(SBFE_VAR_BIN_$(1)_BIN)
 SBFE_FLSH_BIN_$(1)_ARG=$(SBFE_VAR_BIN_$(1)_OFFSET) $(SBFE_VAR_BIN_$(1)_BIN)
-endif
+endif # Flash Encryption disabled
 
 sbfe-flash-$1: $$(SBFE_FLSH_BIN_$(1)_DEP)
 	echo Processing Flashing \'$1\' : $$@
@@ -136,23 +145,13 @@ sbfe-flash-$1: $$(SBFE_FLSH_BIN_$(1)_DEP)
 endif # SBFE_VAR_BIN_$(1)_OFFSET is not null
 endif # SBFE_VAR_BIN_$(1)_BIN is not null
 	
-endef # sbfe-declare-target
+endef # sbfe-declare-binary-target
 
 define sbfe-declare-flash-target
-$(info SBFE declaration of flash target $1)
-$(info - TGT - $(SBFE_VAR_FLSH_$(1)_TARGET))
-$(info - IDX - $(SBFE_VAR_FLSH_$(1)_BIN_IDX))
-$(call sbfe-declare-flash-target-def,$1,$(SBFE_VAR_FLSH_$(1)_BIN_IDX))
-endef
+$(if $(V),$(info SBFE - declaration of flash target $1))
 
-#
-# $1 name
-# $2 list of binary target
-#
-define sbfe-declare-flash-target-def
-
-SBFE_FLSH_TGT_$(1)_DEP=$(foreach bin,$2, $(SBFE_FLSH_BIN_$(bin)_DEP) )
-SBFE_FLSH_TGT_$(1)_ARG=$(foreach bin,$2, $(SBFE_FLSH_BIN_$(bin)_ARG) )
+SBFE_FLSH_TGT_$(1)_DEP=$(foreach bin,$(SBFE_VAR_FLSH_$(1)_BIN_IDX), $(SBFE_FLSH_BIN_$(bin)_DEP) )
+SBFE_FLSH_TGT_$(1)_ARG=$(foreach bin,$(SBFE_VAR_FLSH_$(1)_BIN_IDX), $(SBFE_FLSH_BIN_$(bin)_ARG) )
 
 sbfe-flash-$1: $$(SBFE_FLSH_TGT_$(1)_DEP)
 	echo Processing Flash Target \'$1\' : $$@
